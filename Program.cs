c46cfce1cff3cbd0461ba03bb5862408b3d184f6
Deepak Selvan <dselvan@gmail.com>
2020-04-29 20:21:20 -0500

    Add ProcessSubscriberUpdates which implements a pipeline for reading and processing car updates

--- Program.cs
+++ Program.cs
@@ -1,28 +1,27 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.IO.Pipelines;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using AiTelemetry.UdpReceiver.Datagrams;
 using AiTelemetry.UdpReceiver.Models;
 using AiTelemetry.UdpReceiver.Utils;
 using AutoMapper;
+using System.Buffers;
 
 namespace AiTelemetry.UdpReceiver
 {
     class Program
     {
-        // currently disabling will make main actually use async later
-#pragma warning disable CS1998
         static async Task Main(string[] args)
         {
-#pragma warning restore CS19998
             // udp server configuration
             var serverIpAddress = IPAddress.Parse("192.168.1.53");
             var serverPort = 9996;
             var serverEndpoint = new IPEndPoint(serverIpAddress, serverPort);
 
-            // automapper initialization for mapping interop DTOs => Models 
+            // automapper initialization for mapping interop Datagrams => Models 
             // NTS: Install-Package AutoMapper.Extensions.Microsoft.DependencyInjection
             MapperConfiguration config = new MapperConfiguration(
                 cfg => cfg.CreateMap<HandshakeResponseDatagram, HandshakeResponseModel>()
@@ -41,7 +40,7 @@ namespace AiTelemetry.UdpReceiver
             var requestByteArray = StructSerializer.Serialize(handshakeRequest);
 
             // send request and block for response 
-            var client = new UdpClient();
+            using var client = new UdpClient();
             client.Connect(serverEndpoint);
             client.Send(requestByteArray, Marshal.SizeOf(typeof(TelemetryServerConfigurationDatagram)));
             var responseByteArray = client.Receive(ref serverEndpoint);
@@ -63,18 +62,96 @@ namespace AiTelemetry.UdpReceiver
             requestByteArray = StructSerializer.Serialize(updateRequest);
             client.Send(requestByteArray, Marshal.SizeOf(typeof(TelemetryServerConfigurationDatagram)));
 
-            responseByteArray = client.Receive(ref serverEndpoint);
-            var carData = StructSerializer.Deserialize<CarUpdateDatagram>(responseByteArray, 0);
-
-            var disconnect = new TelemetryServerConfigurationDatagram()
+            Console.CancelKeyPress += (sender, e) =>
             {
-                identifier = PlatformType.Web,
-                operationId = OperationType.Dismiss,
-                version = 1
+                var disconnect = new TelemetryServerConfigurationDatagram()
+                {
+                    identifier = PlatformType.Web,
+                    operationId = OperationType.Dismiss,
+                    version = 1
+                };
+
+                requestByteArray = StructSerializer.Serialize(disconnect);
+                client.Send(requestByteArray, Marshal.SizeOf(typeof(TelemetryServerConfigurationDatagram)));
+                Console.WriteLine("Exiting Gracefully...");
+                Environment.Exit(0);
             };
 
-            requestByteArray = StructSerializer.Serialize(disconnect);
-            client.Send(requestByteArray, Marshal.SizeOf(typeof(TelemetryServerConfigurationDatagram)));
+            await ProcessSubscriberUpdates(client);
+        }
+
+        static async Task ProcessSubscriberUpdates(UdpClient client)
+        {
+            var pipe = new Pipe();
+            var writeTask = FillPipeAsync(client, pipe.Writer);
+            var readTask = ReadPipeAsync(pipe.Reader);
+
+            await Task.WhenAll(writeTask, readTask);
+        }
+
+        static async Task FillPipeAsync(UdpClient client, PipeWriter writer)
+        {
+            // size of a single CarUpdateDatagram
+            const int minBufferSize = 328;
+
+            while (true)
+            {
+                var memory = writer.GetMemory(minBufferSize);
+
+                var udpResult = await client.ReceiveAsync();
+                udpResult.Buffer.CopyTo(memory);
+                writer.Advance(udpResult.Buffer.Length);
+
+                var result = writer.FlushAsync();
+
+                if (result.IsCompleted && false)
+                    break;
+            }
+
+            await writer.CompleteAsync();
+        }
+
+        static async Task ReadPipeAsync(PipeReader reader)
+        {
+            while (true)
+            {
+                var result = await reader.ReadAsync();
+                ReadOnlySequence<byte> buffer = result.Buffer;
+
+                while (TryReadCarUpdateDatagram(ref buffer, out var completeDatagram))
+                {
+                    var carData = StructSerializer
+                        .Deserialize<CarUpdateDatagram>(completeDatagram.ToArray(), 0);
+
+                    // simulate some IO backpressure to see if pipeline works 
+                    // Thread.Sleep(100);
+                    Console.WriteLine(carData.nEngineRpm);
+                }
+
+                reader.AdvanceTo(buffer.Start, buffer.End);
+
+                if (result.IsCompleted)
+                {
+                    break;
+                }
+            }
+
+            await reader.CompleteAsync();
+        }
+
+        static bool TryReadCarUpdateDatagram(ref ReadOnlySequence<byte> buffer, out ReadOnlySequence<byte> completeDatagram)
+        {
+            var size = buffer.Length;
+
+            if (buffer.Length < 328)
+            {
+                completeDatagram = default;
+                return false;
+            }
+
+            completeDatagram = buffer.Slice(0, 328);
+            buffer = buffer.Slice(328, buffer.End);
+            return true;
         }
     }
 }
